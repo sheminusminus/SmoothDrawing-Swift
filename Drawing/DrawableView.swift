8dff777e10efe35050420ca51b3a9c0f48e69e69
Emily <sheminusminus@users.noreply.github.com>
2016-09-08 02:55:40 -0500

    Update DrawableView.swift
    slightly updated version of drawableview.swift (created by nofel mahmood)
    updated for swift 2.3
    original at https://github.com/nofelmahmood/SmoothDrawing-Swift

--- Drawing/DrawableView.swift
+++ Drawing/DrawableView.swift
@@ -1,62 +1,57 @@
-//
-//  DrawableView.swift
-//  Drawing
-//
-//  Created by Nofel Mahmood on 23/01/2015.
-//  Copyright (c) 2015 Nofel Mahmood. All rights reserved.
-//
+// slightly updated version of drawableview.swift (created by nofel mahmood)
+// swift 2.3
+// https://github.com/nofelmahmood/SmoothDrawing-Swift
 
 import UIKit
 
 class DrawableView: UIView {
-
-    let path=UIBezierPath()
-    var previousPoint:CGPoint
-    var lineWidth:CGFloat=0.0
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override init(frame: CGRect) {
-        previousPoint=CGPoint.zeroPoint
-        super.init(frame: frame)
-    }
-
-    required init(coder aDecoder: NSCoder) {
-        previousPoint=CGPoint.zeroPoint
-        super.init(coder: aDecoder)
-        var panGestureRecognizer=UIPanGestureRecognizer(target: self, action: "pan:")
-        panGestureRecognizer.maximumNumberOfTouches=1
-        self.addGestureRecognizer(panGestureRecognizer)
-
-    }
-
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-        UIColor.greenColor().setStroke()
-        path.stroke()
-        path.lineWidth=lineWidth
-    }
-    func pan(panGestureRecognizer:UIPanGestureRecognizer)->Void
-    {
-        var currentPoint=panGestureRecognizer.locationInView(self)
-        let midPoint=self.midPoint(previousPoint, p1: currentPoint)
-        
-        if panGestureRecognizer.state == .Began
-        {
-            path.moveToPoint(currentPoint)
-        }
-        else if panGestureRecognizer.state == .Changed
-        {
-            path.addQuadCurveToPoint(midPoint,controlPoint: previousPoint)
-        }
-        
-        previousPoint=currentPoint
-        self.setNeedsDisplay()
-    }
-    func midPoint(p0:CGPoint,p1:CGPoint)->CGPoint
-    {
-        let x=(p0.x+p1.x)/2
-        let y=(p0.y+p1.y)/2
-        return CGPoint(x: x, y: y)
-    }
-
+	
+	let path = UIBezierPath()
+	var previousPoint: CGPoint
+	var lineWidth: CGFloat = 5.0
+	
+	override init(frame: CGRect) {
+		previousPoint = CGPoint.zero
+		super.init(frame: frame)
+	}
+	
+	required init(coder aDecoder: NSCoder) {
+		previousPoint = CGPoint.zero
+		super.init(coder: aDecoder)!
+		let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(DrawableView.pan(_:)))
+		panGestureRecognizer.maximumNumberOfTouches = 1
+		self.addGestureRecognizer(panGestureRecognizer)
+		
+	}
+	
+	override func drawRect(rect: CGRect) {
+		// Drawing code
+		UIColor.whiteColor().setStroke()
+		path.stroke()
+		path.lineWidth = lineWidth
+	}
+	
+	func pan(panGestureRecognizer:UIPanGestureRecognizer)->Void {
+		let currentPoint = panGestureRecognizer.locationInView(self)
+		let midPoint = self.midPoint(previousPoint, p1: currentPoint)
+		
+		if panGestureRecognizer.state == .Began
+		{
+			path.moveToPoint(currentPoint)
+		}
+		else if panGestureRecognizer.state == .Changed
+		{
+			path.addQuadCurveToPoint(midPoint,controlPoint: previousPoint)
+		}
+		
+		previousPoint = currentPoint
+		self.setNeedsDisplay()
+	}
+	
+	func midPoint(p0:CGPoint,p1:CGPoint)->CGPoint {
+		let x = (p0.x+p1.x) / 2
+		let y = (p0.y+p1.y) / 2
+		return CGPoint(x: x, y: y)
+	}
+	
 }
